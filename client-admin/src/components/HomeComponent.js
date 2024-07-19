8ecf63bebb194dffd4910f396eb1890d9dfb214e
Vinbobo <128805837+Vinbobo@users.noreply.github.com>
2024-07-19 18:53:24 +0700

    Update HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -5,7 +5,7 @@ class Home extends Component {
     return (
       <div className="align-center">
         <h2 className="text-center">CHÀO MỪNG TỚI </h2>
-        <img src="https://voz.vn/attachments/tu-hao-hat-mai-len-vn-oi-gif.2298957/" width="800px" height="600px" alt="Home" />
+        <img src="https://ss-images.saostar.vn/2023/10/6/pc/1696567227754/8gviun9r6y1-yielcdcpel2-lem1i840aa3.gif" width="800px" height="600px" alt="Home" />
       </div>
     );
   }
