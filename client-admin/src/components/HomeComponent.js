ec9260bb5217cf0fe5156a6e35fad7ebf14f646f
Vinbobo <128805837+Vinbobo@users.noreply.github.com>
2024-07-20 13:26:08 +0700

    Update HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div className="align-center">
-        <h2 className="text-center">CHÀO MỪNG TỚI </h2>
+        <h2 className="text-center">CHÀO MỪNG TỚI TRANG ADMIN FASHIONSỐP</h2>
         <img src="https://d1xchyov513y0i.cloudfront.net/wp-content/uploads/2020/06/05123906/storefront-2.gif" width="800px" height="600px" alt="Home" />
       </div>
     );
